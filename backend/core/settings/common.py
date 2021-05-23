a80105feefcfdaa58001e79f95d07393ac0f6a3a
Sergey Konik <s.konik.job@gmail.com>
2021-05-23 13:06:29 +0300

    setup: add Makefile, refactor and upgrade python to 3.9
    * added Makefile and several make commands like `make dev.up`, `make dev.migrate`, etc to make development more convenient;
    * upgraded python from 3.7 to 3.9 to keep python fresh;
    * added flake8 linter to integrate in future ci/cd;
    * added postgres support along with required libraries;
    * updated docs to add basic understanding how to run the project.
    
    Refs: #1

--- backend/core/settings/common.py
+++ backend/core/settings/common.py
@@ -97,10 +97,7 @@ WSGI_APPLICATION = env.str(
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
--- docker/backend/dev.Dockerfile
+++ docker/backend/dev.Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7 AS builder
+FROM python:3.9-slim AS builder
 
 ENV PYTHONUNBUFFERED=1 \
     # prevents python creating .pyc files
