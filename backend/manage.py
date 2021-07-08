081693ca223a00de12b4376347d0720374730f21
Sergey Konik <50069473+skonik@users.noreply.github.com>
2021-07-08 21:40:17 +0300

    [#10] devops(CI): add circle ci integration (#11)

--- backend/manage.py
+++ backend/manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# type: ignore
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -6,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.develop')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
